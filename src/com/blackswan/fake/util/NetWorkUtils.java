6edad1d0f4c23cd6ed7d208f22ba3d782cabed86
ChenSongSong <512709720@qq.com>
2014-11-07 14:32:51 +0800

    add imagesCache

--- src/com/blackswan/fake/util/NetWorkUtils.java
+++ src/com/blackswan/fake/util/NetWorkUtils.java
@@ -4,14 +4,6 @@ import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo.State;
 
-/**
- * @fileName NetWorkUtils.java
- * @package com.immomo.momo.android.util
- * @description 网络工具类
- * @author 任东卫
- * @email 86930007@qq.com
- * @version 1.0
- */
 public class NetWorkUtils {
 	private Context mContext;
 	public State wifiState = null;
